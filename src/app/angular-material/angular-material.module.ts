25d88bee881223837b062de3b12a060420248046
unknown <Abdalrhmann323@gmail.com>
2020-11-07 07:51:41 +0200

    Refactor -- seprating material module

---
+++ src/app/angular-material/angular-material.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatDialogModule} from '@angular/material/dialog';
+
+
+@NgModule({
+  declarations: [],
+  // #Note A feature angler offers is that you can remove the import array and just export them and re-importing 
+  // , will be done automatically by angular
+  
+  // imports: [
+  //   MatInputModule,
+  //   MatCardModule,
+  //   MatButtonModule,
+  //   MatToolbarModule,
+  //   MatExpansionModule,
+  //   MatProgressSpinnerModule,
+  //   MatPaginatorModule,
+  //   MatDialogModule,
+  //   CommonModule
+  // ],
+  // by default these imports not exported to other modules
+  // so, to make them usable in another module we need to export them
+  exports:[
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatExpansionModule,
+    MatProgressSpinnerModule,
+    MatPaginatorModule,
+    MatDialogModule,
+    CommonModule
+  ]
+})
+export class AngularMaterialModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,16 +9,10 @@ import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http'
 
-import {MatInputModule} from '@angular/material/input';
-import {MatCardModule} from '@angular/material/card';
-import {MatButtonModule} from '@angular/material/button';
-import { HeaderComponent } from './header/header.component';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import {AngularMaterialModule} from './angular-material/angular-material.module'
+
 import {PostListComponent } from './posts/post-list/post-list.component';
-import {MatExpansionModule} from '@angular/material/expansion';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-import {MatPaginatorModule} from '@angular/material/paginator';
-import {MatDialogModule} from '@angular/material/dialog';
+import { HeaderComponent } from './header/header.component';
 
 
 import { LoginComponent } from './auth/components/login/login.component';
@@ -41,15 +35,8 @@ import {ErrorComponent} from'./error/error.component'
     ReactiveFormsModule,
     FormsModule,
     BrowserAnimationsModule,
-
-    MatInputModule,
-    MatCardModule,
-    MatButtonModule,
-    MatToolbarModule,
-    MatExpansionModule,
-    MatProgressSpinnerModule,
-    MatPaginatorModule,
-    MatDialogModule,
+    AngularMaterialModule,
+   
 
     HttpClientModule,
     AppRoutingModule
